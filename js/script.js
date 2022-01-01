c7992a73652a0f6acd0473b0f9adb496d9f02c19
Duong29ds <luuduong29112000@gmail.com>
2022-01-01 17:34:54 +0700

    second-commit

--- js/script.js
+++ js/script.js
@@ -10,3 +10,27 @@ const header = document.querySelector(".header");
 btnNav.addEventListener("click", function () {
   header.classList.toggle("nav-open");
 });
+
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (ent.isIntersecting === false) {
+      document.querySelector(".header").classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.querySelector(".header").classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
